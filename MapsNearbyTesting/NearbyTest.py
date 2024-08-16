35b93ff1ce60e2e4ffbfb472bae03ec37fdfb335
Griffmos <97399585+Griffmos@users.noreply.github.com>
2024-08-16 14:33:09 -0400

    Got google api to find nearby places working

---
+++ MapsNearbyTesting/NearbyTest.py
@@ -0,0 +1,33 @@
+import googlemaps
+
+def miles_to_meters(miles):
+    '''Helper function for conversions'''
+    try:
+        return miles * 1609.34
+    except:
+        return -1
+    
+API_KEY = open('API_KEY.txt', 'r').read()
+
+map_client = googlemaps.Client(API_KEY)
+
+location = (40.31302063254212, -74.62031783336619)
+
+distance = miles_to_meters(0.05)
+
+place_list = []
+
+response = map_client.places_nearby(
+    location = location,
+    radius = distance
+)
+
+
+print(response.get("status"))
+
+
+place_list.extend(response.get('results'))
+
+
+for place in place_list:
+    print(place.get('name'))
