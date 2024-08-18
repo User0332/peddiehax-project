6faa0fc4e8e3ec55bc0909fe0a35d4b9cecf008c
Brendan Hirshorn <brendan.l.hirshorn@gmail.com>
2024-08-18 01:34:51 -0400

    Action ring animation

--- src/app/static/js/actiontabs.js
+++ src/app/static/js/actiontabs.js
@@ -3,3 +3,42 @@ let tab = document.querySelector('.tab');
 menuToggle.onclick = function(){
     tab.classList.toggle('active');
 }
+
+const circularBG1 = document.createElement('div');
+circularBG1.className = "actions";
+
+const actionRing = document.createElement('ul');
+actionRing.className = "action-ring";
+
+// Add action options -- TODO ADD CLICK ACTIONS
+
+// Example Actions -- NEEDS FUNCTIONALITY -- JUST LOOKS PRETTY RN
+
+const icons = [
+   "add-person",
+    "add-text",
+    "add-location"
+]
+
+for (const imgfile of icons) {
+    const action = document.createElement('li');
+    const anchor = document.createElement('a');
+    anchor.href = "";
+    const img = document.createElement('img');
+    img.src = `/static/images/${imgfile}.svg`;
+    anchor.appendChild(img);
+    action.append(anchor);
+    actionRing.appendChild(action);
+}
+
+circularBG1.appendChild(actionRing);
+
+const circularBG2 = document.createElement('div');
+circularBG2.className = "actions-bg-1";
+
+const circularBG3 = document.createElement('div');
+circularBG3.className = "actions-bg-2";
+
+tab.appendChild(circularBG1);
+tab.appendChild(circularBG2);
+tab.appendChild(circularBG3);
